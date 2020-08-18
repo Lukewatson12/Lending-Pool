92711138d9a81ed66835d04687f4cc34d01e4c21
Luke Watson <>
2020-08-18 16:12:52 +0800

    init

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,7 @@
+const MetaCoin = artifacts.require("MetaCoin");
+
+module.exports = function(deployer) {
+  deployer.deploy(ConvertLib);
+  deployer.link(ConvertLib, MetaCoin);
+  deployer.deploy(MetaCoin);
+};
