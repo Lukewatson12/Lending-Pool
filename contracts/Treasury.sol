9abbb7d91224492d5e6ab023c45fd8db99de6542
Luke Watson <lukewatson@Lukes-MacBook-Pro.local>
2020-08-19 11:38:45 +0800

    Update treasury code

--- contracts/Treasury.sol
+++ contracts/Treasury.sol
@@ -12,6 +12,10 @@ contract Treasury {
 
     event DepositEvent(uint _amount, address _from, uint when);
 
+    event WithdrawalEvent(uint _amount, address _to, uint when);
+
+    event DepositWithdrawnEvent(uint _amount, address _to, uint when, bool isDepositEmpty);
+
     modifier withdrawalLimit(uint _amount, address _address) {
         uint totalDeposited = getDepositsForAddress(_address);
         require(totalDeposited >= _amount, "Total withdrawal exceeds total deposited");
@@ -24,9 +28,7 @@ contract Treasury {
         uint _timeDeposit;
     }
 
-    mapping(
-    address => DepositStruct[]
-    ) private deposits;
+    mapping(address => DepositStruct[]) public deposits;
 
     Pool private pool;
 
@@ -34,14 +36,19 @@ contract Treasury {
         pool = _pool;
     }
 
-    function deposit(uint _amount, address _address, uint _timeDeposit) public {
+    receive() payable external {
+        deposit(msg.sender);
+    }
+
+    function deposit(address _address) payable public {
+        uint timeDeposit = block.timestamp * 14;
         deposits[_address].push(DepositStruct(
-                _amount,
+                msg.value,
                 _address,
-                _timeDeposit
+                timeDeposit
             ));
 
-        emit DepositEvent(_amount, _address, _timeDeposit);
+        emit DepositEvent(msg.value, _address, timeDeposit);
     }
 
     function getDepositsForAddress(address _address) public view returns (uint amount) {
@@ -56,20 +63,44 @@ contract Treasury {
         return totalDeposited;
     }
 
-    function withdraw(address _address, uint _amount) external withdrawalLimit(_amount, _address) {
-        uint totalWithdrawn;
+    function withdraw(address payable _address, uint _amount) external withdrawalLimit(_amount, _address) {
+        handleFundBalancing(_address, _amount);
 
+        emit WithdrawalEvent(_amount, _address, block.timestamp * 14);
+
+        _address.transfer(_amount);
+    }
+
+
+    // Could use a refactor
+    function handleFundBalancing(address payable _address, uint _amountToWithdraw) internal {
+        uint amountLeftToWithdraw = _amountToWithdraw;
+        uint when = block.timestamp * 14;
+
+        // Grab each deposit for address
         for (uint i = 0; i < deposits[_address].length; i++) {
+            // Get amount in the deposit
             uint amountInDeposit = deposits[_address][i]._amount;
 
-            if (amountInDeposit >= totalWithdrawn) {
-                // need to caclulate remainder
+            // If the total amount in the deposit is equal or greater than the amount to withdraw we calculate the amount remaining
+            if (amountInDeposit >= amountLeftToWithdraw) {
+                uint remainder = amountInDeposit.sub(amountLeftToWithdraw);
+
+                // If there is a remainder update the deposit
+                if (remainder > 0) {
+                    emit DepositWithdrawnEvent(amountLeftToWithdraw, _address, when, false);
+                    deposits[_address][i]._amount = remainder;
+                } else {
+                    emit DepositWithdrawnEvent(amountLeftToWithdraw, _address, when, true);
+                    delete deposits[_address][i];
+                }
+                break;
             }
 
-            totalWithdrawn = totalWithdrawn.add(amountInDeposit);
-
-//            deposits[_address][i].pop();
+            // Subtract the amount in the deposit from the amount to withdraw
+            amountLeftToWithdraw = amountLeftToWithdraw.sub(amountInDeposit);
+            emit DepositWithdrawnEvent(amountInDeposit, _address, when, true);
+            delete deposits[_address][i];
         }
-
     }
 }
